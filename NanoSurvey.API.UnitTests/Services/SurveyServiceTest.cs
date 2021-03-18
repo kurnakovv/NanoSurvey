b1c4fd5245e1d0e6893175143a442d3010e8ddda
KurnakovMaksim <maksk210@gmail.com>
2021-03-18 15:51:47 +0900

    Add Unit test for SurveyController

---
+++ NanoSurvey.API.UnitTests/Services/SurveyServiceTest.cs
@@ -0,0 +1,29 @@
+﻿using Moq;
+using NanoSurvey.API.Entities;
+using NanoSurvey.API.Services.Abstract;
+using Xunit;
+
+namespace NanoSurvey.API.UnitTests.Services
+{
+    public class SurveyServiceTest
+    {
+        private readonly Question question = new Question() 
+        { 
+            Title = "Title",
+        };
+
+        [Fact]
+        public void GetQuestionById_GetQuestionByValidId_Question()
+        {
+            var surveyService = new Mock<ISurveyService>();
+
+            var result = surveyService.Setup(s 
+                => s.GetQuestionById(1))
+                    .ReturnsAsync(question);
+
+            Assert.NotNull(result);
+            Assert.Equal(question.Title,
+                surveyService.Object.GetQuestionById(1).Result.Title);
+        }
+    }
+}
